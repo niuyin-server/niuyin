4a81749a5e5e1d99d970f54befb79ce7f6319f36
roydonGuo <3133010060@qq.com>
2023-11-18 15:28:47 +0800

    根据视频id查询被哪些收藏夹收藏

--- niuyin-service/niuyin-service-behave/src/main/java/com/niuyin/service/behave/controller/v1/UserFavoriteVideoController.java
+++ niuyin-service/niuyin-service-behave/src/main/java/com/niuyin/service/behave/controller/v1/UserFavoriteVideoController.java
@@ -1,10 +1,14 @@
 package com.niuyin.service.behave.controller.v1;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.niuyin.common.context.UserContext;
 import com.niuyin.common.domain.R;
+import com.niuyin.model.behave.domain.UserFavorite;
 import com.niuyin.model.behave.domain.UserFavoriteVideo;
 import com.niuyin.model.behave.dto.UserFavoriteVideoDTO;
 import com.niuyin.service.behave.service.IUserFavoriteVideoService;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,9 +26,24 @@ public class UserFavoriteVideoController {
     @Resource
     private IUserFavoriteVideoService userFavoriteVideoService;
 
+    /**
+     * 收藏视频
+     */
     @PostMapping()
-    public R<Boolean> getDetails(@RequestBody UserFavoriteVideoDTO userFavoriteVideoDTO) {
+    public R<Boolean> favoriteVideoToCollection(@Validated @RequestBody UserFavoriteVideoDTO userFavoriteVideoDTO) {
         return R.ok(userFavoriteVideoService.videoFavorites(userFavoriteVideoDTO));
+    }
+
+    /**
+     * 根据视频id查询被哪些收藏夹收藏
+     */
+    @GetMapping("/{videoId}")
+    public R<Long[]> getVideoUserCollection(@PathVariable("videoId") String videoId) {
+        LambdaQueryWrapper<UserFavoriteVideo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(UserFavoriteVideo::getUserId, UserContext.getUserId());
+        queryWrapper.eq(UserFavoriteVideo::getVideoId, videoId);
+        Long[] array = userFavoriteVideoService.list(queryWrapper).stream().map(UserFavoriteVideo::getFavoriteId).toArray(Long[]::new);
+        return R.ok(array);
     }
 
 }
