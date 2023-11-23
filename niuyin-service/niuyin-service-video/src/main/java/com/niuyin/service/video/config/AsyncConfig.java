6e1077d42796995264a800907baa3b5224d33a03
roydonGuo <3133010060@qq.com>
2023-11-23 12:02:50 +0800

    异步非阻塞线程池优化feed接口

---
+++ niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/config/AsyncConfig.java
@@ -0,0 +1,31 @@
+package com.niuyin.service.video.config;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10); // 核心线程数
+        executor.setMaxPoolSize(20); // 最大线程数
+        executor.setQueueCapacity(100); // 队列容量
+        executor.setThreadNamePrefix("Async-"); // 线程名称前缀
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+}
