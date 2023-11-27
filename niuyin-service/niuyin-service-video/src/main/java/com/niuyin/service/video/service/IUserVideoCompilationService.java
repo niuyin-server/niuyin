a6df1d37d3f5e572494988a3231d3caae60b55e6
roydonGuo <3133010060@qq.com>
2023-11-27 18:20:16 +0800

    视频合集

---
+++ niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/service/IUserVideoCompilationService.java
@@ -0,0 +1,14 @@
+package com.niuyin.service.video.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.niuyin.model.video.domain.UserVideoCompilation;
+
+/**
+ * 用户视频合集表(UserVideoCompilation)表服务接口
+ *
+ * @author roydon
+ * @since 2023-11-27 18:08:39
+ */
+public interface IUserVideoCompilationService extends IService<UserVideoCompilation> {
+
+}
