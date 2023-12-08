3201960919a4bed677bd6acab2dac329153e38d7
roydon <3133010060@qq.com>
2023-12-08 11:40:31 +0800

    improve 根据文件类型限制大小

--- niuyin-starter/niuyin-starter-file/src/main/java/com/niuyin/starter/file/util/file/FileUploadUtils.java
+++ niuyin-starter/niuyin-starter-file/src/main/java/com/niuyin/starter/file/util/file/FileUploadUtils.java
@@ -9,9 +9,13 @@ import org.springframework.web.multipart.MultipartFile;
  */
 public class FileUploadUtils {
     /**
-     * 默认最大值 100M
+     * 图片默认最大值 5M
      */
-    public static final long DEFAULT_MAX_SIZE = 100 * 1024 * 1024;
+    public static final long DEFAULT_IMAGE_MAX_SIZE = 5 * 1024 * 1024;
+    /**
+     * 视频默认最大值 100M
+     */
+    public static final long DEFAULT_VIDEO_MAX_SIZE = 100 * 1024 * 1024;
 
     // 用户头像大小限制
     public static final long MEMBER_AVATAR_MAX_SIZE = 1024 * 1024;
@@ -21,10 +25,22 @@ public class FileUploadUtils {
      *
      * @param file 上传的文件
      */
-    public static final void assertAllowed(MultipartFile file, String[] allowedExtension) {
+    public static final void assertImageAllowed(MultipartFile file, String[] allowedExtension) {
         long size = file.getSize();
-        if (size > DEFAULT_MAX_SIZE) {
-            throw new RuntimeException("文件大小超出限制：" + DEFAULT_MAX_SIZE / 1024 / 1024 + "M");
+        if (size > DEFAULT_IMAGE_MAX_SIZE) {
+            throw new RuntimeException("图片大小超出限制：" + DEFAULT_IMAGE_MAX_SIZE / 1024 / 1024 + "M");
+        }
+    }
+
+    /**
+     * 文件大小校验
+     *
+     * @param file 上传的文件
+     */
+    public static final void assertVideoAllowed(MultipartFile file, String[] allowedExtension) {
+        long size = file.getSize();
+        if (size > DEFAULT_VIDEO_MAX_SIZE) {
+            throw new RuntimeException("视频大小超出限制：" + DEFAULT_VIDEO_MAX_SIZE / 1024 / 1024 + "M");
         }
     }
 
