94959e0329aa826e116459aafe9e97f4a3c05ffc
roydon <3133010060@qq.com>
2023-12-30 00:41:06 +0800

    视频合集操作

---
+++ niuyin-model/src/main/java/com/niuyin/model/video/dto/UpdateUserVideoCompilationDTO.java
@@ -0,0 +1,35 @@
+package com.niuyin.model.video.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+/**
+ * UpdateUserVideoCompilationDTO
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/12/28
+ **/
+@Data
+public class UpdateUserVideoCompilationDTO {
+    /**
+     * compilation_id
+     */
+    private Long compilationId;
+    /**
+     * 合集标题
+     */
+    @NotBlank(message = "标题不能为空")
+    @Size(min = 1, max = 20, message = "标题需在20字符以内")
+    private String title;
+    /**
+     * 描述
+     */
+    @Size(min = 1, max = 200, message = "描述需在200字符以内")
+    private String description;
+    /**
+     * 合集封面(5M)
+     */
+    private String coverImage;
+}
