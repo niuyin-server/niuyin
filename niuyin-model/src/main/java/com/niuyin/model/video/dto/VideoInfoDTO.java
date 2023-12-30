25023d245fb84952d09ee8fc8ed679699b718ac9
roydon <3133010060@qq.com>
2023-12-30 21:58:26 +0800

    根据视频远程url获取视频详情

---
+++ niuyin-model/src/main/java/com/niuyin/model/video/dto/VideoInfoDTO.java
@@ -0,0 +1,14 @@
+package com.niuyin.model.video.dto;
+
+import lombok.Data;
+
+/**
+ * VideoInfoDTO
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2023/12/30
+ **/
+@Data
+public class VideoInfoDTO {
+    private String videoUrl;
+}
