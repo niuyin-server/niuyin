ab2e1b2a180dce30db6897c0563049ee809d3e82
roydon <3133010060@qq.com>
2024-01-05 01:43:45 +0800

    异步任务线程池

--- niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/config/AsyncConfig.java
+++ niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/config/AsyncConfig.java
@@ -16,9 +16,9 @@ public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(10); // 核心线程数
-        executor.setMaxPoolSize(20); // 最大线程数
-        executor.setQueueCapacity(100); // 队列容量
+        executor.setCorePoolSize(16); // 核心线程数
+        executor.setMaxPoolSize(100); // 最大线程数
+        executor.setQueueCapacity(1000); // 队列容量
         executor.setThreadNamePrefix("Async-"); // 线程名称前缀
         executor.initialize();
         return executor;
