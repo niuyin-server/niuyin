fdcb75ceda412d0324a8baf7e65d771aec0f6bbb
roydon <3133010060@qq.com>
2024-01-15 22:51:02 +0800

    用户模型field

--- niuyin-model/src/main/java/com/niuyin/model/video/vo/UserModelField.java
+++ niuyin-model/src/main/java/com/niuyin/model/video/vo/UserModelField.java
@@ -2,6 +2,8 @@ package com.niuyin.model.video.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 用户模型field
  *
@@ -9,8 +11,8 @@ import lombok.Data;
  * @DATE: 2023/12/6
  **/
 @Data
-public class UserModelField {
-    private String tag;
-    private Long videoId;
+public class UserModelField implements Serializable {
+    private static final long serialVersionUID = 785378785L;
+    private Long tagId;
     private Double score;
 }
