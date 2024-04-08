0572fa716e37302ebf545685cdcd4f1860224783
roydon <3133010060@qq.com>
2024-04-08 13:16:49 +0800

    @EnableScheduling

--- niuyin-service/niuyin-service-notice/src/main/java/com/niuyin/service/notice/NoticeApplication.java
+++ niuyin-service/niuyin-service-notice/src/main/java/com/niuyin/service/notice/NoticeApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * NoticeService
@@ -23,6 +24,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableUserTokenInterceptor
 @EnableRedisConfig
 @EnableAsync
+@EnableScheduling
 @Import({MybatisPlusConfig.class, Swagger2Configuration.class})
 public class NoticeApplication {
     public static void main(String[] args) {
