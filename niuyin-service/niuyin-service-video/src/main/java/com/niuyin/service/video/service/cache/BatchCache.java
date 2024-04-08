0c81be99a216020bf25d12c39492d8537aa7c479
roydon <3133010060@qq.com>
2024-04-08 13:26:13 +0800

    BatchCache

---
+++ niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/service/cache/BatchCache.java
@@ -0,0 +1,26 @@
+package com.niuyin.service.video.service.cache;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BatchCache<IN, OUT> {
+    /**
+     * 获取单个
+     */
+    OUT get(IN req);
+
+    /**
+     * 获取批量
+     */
+    Map<IN, OUT> getBatch(List<IN> req);
+
+    /**
+     * 修改删除单个
+     */
+    void delete(IN req);
+
+    /**
+     * 修改删除多个
+     */
+    void deleteBatch(List<IN> req);
+}
