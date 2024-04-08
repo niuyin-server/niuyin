7336d54045158eb40c3591240ed74b002564629f
roydon <3133010060@qq.com>
2024-04-08 13:13:30 +0800

    improve

---
+++ niuyin-service/niuyin-service-social/src/test/java/com/niuyin/service/social/UserFollowServiceTest.java
@@ -0,0 +1,31 @@
+package com.niuyin.service.social;
+
+import com.niuyin.common.domain.vo.PageDataInfo;
+import com.niuyin.model.common.dto.PageDTO;
+import com.niuyin.model.video.vo.VideoVO;
+import com.niuyin.service.social.service.IUserFollowService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+/**
+ * UserFollowServiceTest
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2024/4/7
+ **/
+@Slf4j
+@SpringBootTest
+public class UserFollowServiceTest {
+
+    @Resource
+    private IUserFollowService userFollowService;
+
+    @Test
+    public void test() {
+        PageDataInfo<VideoVO> socialDynamicVideoPage = userFollowService.getSocialDynamicVideoPage(new PageDTO());
+        socialDynamicVideoPage.toString();
+    }
+}
