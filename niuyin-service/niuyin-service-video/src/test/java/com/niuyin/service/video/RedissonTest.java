32e7fc84385a5293d3e1d3c745dc24ac0b610097
roydon <3133010060@qq.com>
2024-04-08 13:16:13 +0800

    del

---
+++ niuyin-service/niuyin-service-video/src/test/java/com/niuyin/service/video/RedissonTest.java
@@ -0,0 +1,48 @@
+//package com.niuyin.service.video;
+//
+//import com.roydon.starter.ratelimit.annotation.RedissonLock;
+//import com.roydon.starter.ratelimit.service.redisson.LockService;
+//import lombok.extern.slf4j.Slf4j;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import javax.annotation.Resource;
+//
+///**
+// * RedissonTest
+// *
+// * @AUTHOR: roydon
+// * @DATE: 2024/2/18
+// **/
+//@Slf4j
+//@SpringBootTest
+//public class RedissonTest {
+//
+//    @Resource
+//    private LockService lockService;
+//
+//    @Test
+//    @RedissonLock(prefixKey = "redisson:lock", key = "test")
+//    void testRedissonAnnoLock() throws InterruptedException {
+//        System.out.println("testRedissonAnnoLock begin");
+//        Thread.sleep(100000);
+//        System.out.println("testRedissonAnnoLock end");
+//        System.out.println();
+//    }
+////
+////    @SneakyThrows
+////    @Test
+////    void lockTest() {
+////        lockService.executeWithLock("redisson:test:lock", 1, TimeUnit.MINUTES, () -> {
+////            System.out.println("testRedissonAnnoLock begin");
+////            try {
+////                Thread.sleep(100000);
+////            } catch (InterruptedException e) {
+////                throw new RuntimeException(e);
+////            }
+////            System.out.println("testRedissonAnnoLock end");
+////            return null;
+////        });
+////    }
+//
+//}
