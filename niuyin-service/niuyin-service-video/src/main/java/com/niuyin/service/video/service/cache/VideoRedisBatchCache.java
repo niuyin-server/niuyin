fa10470cc93527aff4c889cefb77ad05f515951f
roydon <3133010060@qq.com>
2024-04-10 17:47:03 +0800

    VideoRedisBatchCache

---
+++ niuyin-service/niuyin-service-video/src/main/java/com/niuyin/service/video/service/cache/VideoRedisBatchCache.java
@@ -0,0 +1,42 @@
+package com.niuyin.service.video.service.cache;
+
+import com.niuyin.model.video.domain.Video;
+import com.niuyin.service.video.service.IVideoService;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * VideoRedisBatchCache
+ *
+ * @AUTHOR: roydon
+ * @DATE: 2024/4/8
+ **/
+@Service
+public class VideoRedisBatchCache extends AbstractRedisStringCache<String, Video> {
+
+    @Resource
+    @Lazy
+    private IVideoService videoService;
+
+    @Override
+    protected String getKey(String videoId) {
+        return "video:video:" + videoId;
+    }
+
+    @Override
+    protected Long getExpireSeconds() {
+        return 12 * 60 * 60L;// 12小时
+    }
+
+    @Override
+    protected Map<String, Video> load(List<String> videoIds) {
+        List<Video> videoList = videoService.listByIds(videoIds);
+        return videoList.stream().collect(Collectors.toMap(Video::getVideoId, Function.identity()));
+    }
+}
